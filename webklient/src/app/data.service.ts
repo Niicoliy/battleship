3e614fbd67fea99846e2fb1e64bbd20048834e98
Robert Sand <s164911@student.dtu.dk>
2018-04-24 10:17:51 +0200

    Updated website with loog-alike login and highscores.

--- webklient/src/app/data.service.ts
+++ webklient/src/app/data.service.ts
@@ -4,12 +4,16 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 @Injectable()
 export class DataService {
 
-  private goals = new BehaviorSubject<any>(['The initial goal', 'Another sille life goal']);
+  private goals = new BehaviorSubject<any>(['Get this game finished', 'Make the highscore', 'Beat Mikkel!']);
   goal = this.goals.asObservable();
+
+  private highscores = new BehaviorSubject<any>(['s164911 4 wins / 0 looses', 's164914 3 wins / 1 looses', 's153795 1 win / 4 looses']);
+  highscore = this.highscores.asObservable();
 
   constructor() { }
 
   changeGoal(goal) {
     this.goals.next(goal);
+
   }
 }
