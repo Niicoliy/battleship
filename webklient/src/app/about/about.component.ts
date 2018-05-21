69a25cb14b19c2881712fd8a527152a75aea26c9
Robert Sand <s164911@student.dtu.dk>
2018-05-21 17:58:14 +0200

    Testing one form of REST on the About page

--- webklient/src/app/about/about.component.ts
+++ webklient/src/app/about/about.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-about',
@@ -12,16 +13,22 @@ export class AboutComponent implements OnInit {
 
   goals: any;
 
-  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService) {
+  constructor(private route: ActivatedRoute, private router: Router, private _data: DataService, private http: HttpClient) {
     this.route.params.subscribe(res => console.log(res.id));
   }
 
-  ngOnInit() {
+  ngOnInit(): void   {
     this._data.goal.subscribe(res => this.goals = res);
+
+    this.http.get('http://ubuntu4.saluton.dk:47714/rest/resources/game/game/games').subscribe(data => {
+      console.log(data);
+    })
   }
 
   sendMeHome() {
     this.router.navigate(['']);
   }
+
+
 
 }
